e042e421722ad30c69a40ee964d228a21209ebe7
luki27 <lukasz.jakub.skowronek@gmail.com>
2017-07-27 21:23:35 +0200

    initial commit

---
+++ ytGit/src/main/java/ytGit/ytGit/App.java
@@ -0,0 +1,13 @@
+package ytGit.ytGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
