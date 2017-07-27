e16ee1bf94f7e0a4a0e51468ef9d9939bc98d719
luki27 <lukasz.jakub.skowronek@gmail.com>
2017-07-27 21:29:17 +0200

    Added tree code

--- ytGit/src/main/java/ytGit/ytGit/App.java
+++ ytGit/src/main/java/ytGit/ytGit/App.java
@@ -8,6 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       tree(12);
     }
+    
+    public static void tree(int row) {
+		for (int i = 0; i < row; i++) {
+			for (int j = i + 1; j < row; j++) {
+				System.out.print(" ");
+			}
+			for (int k = row - i - 1; k < row+i ; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
 }
